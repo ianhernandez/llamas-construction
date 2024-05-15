4d73bd3c41605503c27e4d7b6c2022bba85e7ddc
Ian Hernandez <ianhernandez23@gmail.com>
2024-05-15 16:03:07 -0700

    add edge rendering to root layout

--- app/root.tsx
+++ app/root.tsx
@@ -7,6 +7,8 @@ import {
 } from "@remix-run/react";
 import { Analytics } from "@vercel/analytics/react";
 
+export const config = { runtime: 'edge' };
+
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
